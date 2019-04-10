f0a83dd3fb508ac4f92738b6d3ea5642072e82e9
pedroromn <pedroromn@gmail.com>
2019-04-09 23:52:03 -0500

    09/04/2019 23:51

---
+++ jhtp/Addition.java
@@ -0,0 +1,24 @@
+// Addition program that displays  the sum of two numbers
+import java.util.Scanner; // program uses class Scanner
+
+public class Addition{
+    public static void main(String[] args) {
+        
+        // create Scanner object to obtain input from the command window
+        Scanner input = new Scanner(System.in);
+
+        int number1;
+        int number2;
+        int sum;
+
+        System.out.print("Enter first integer: ");
+        number1 = input.nextInt();
+
+        System.out.print("Enter second integer: ");
+        number2 = input.nextInt();
+
+        sum = number1 + number2;
+
+        System.out.printf("Sum is %d\n", sum);
+    }
+}
